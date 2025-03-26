8dade19923acf1cee62d7988fba41ddcffd6d044
Diya Srivastava <diyasrivastava2023@gmail.com>
2025-03-26 19:36:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=5703f8c8"
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=5703f8c8"
 
 
 
